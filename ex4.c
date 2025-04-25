bc81011696e18a9111fb4988a6e4a524dbc2aa80
CSI-BIU <csi.biu2024@gmail.com>
2025-04-25 21:08:42 +0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -1,8 +1,9 @@
 /******************
-Name:
-ID:
-Assignment:
+Name: 
+ID: 
+Assignment: ex4
 *******************/
+
 #include <stdio.h>
 #include <string.h>
 
@@ -10,77 +11,57 @@ void task1RobotPaths();
 void task2HumanPyramid();
 void task3ParenthesisValidator();
 void task4QueensBattle();
-void task5CrosswordGenerator();
 
-int main()
-{
+int main() {
     int task = -1;
-    do
-    {
+    do {
         printf("Choose an option:\n"
                "1. Robot Paths\n"
                "2. The Human Pyramid\n"
                "3. Parenthesis Validation\n"
                "4. Queens Battle\n"
-               "5. Crossword Generator\n"
-               "6. Exit\n");
+               "5. Exit\n");
 
-        if (scanf("%d", &task))
-        {
-            switch (task)
-            {
-            case 6:
-                printf("Goodbye!\n");
-                break;
-            case 1:
-                task1RobotPaths();
-                break;
-            case 2:
-                task2HumanPyramid();
-                break;
-            case 3:
-                task3ParenthesisValidator();
-                break;
-            case 4:
-                task4QueensBattle();
-                break;
-            case 5:
-                task5CrosswordGenerator();
-                break;
-            default:
-                printf("Please choose a task number from the list.\n");
-                break;
+        if (scanf("%d", &task)) { // To make sure the input is an integer
+            switch (task) {
+                case 1:
+                    task1RobotPaths();
+                    break;
+                case 2:
+                    task2HumanPyramid();
+                    break;
+                case 3:
+                    task3ParenthesisValidator();
+                    break;
+                case 4:
+                    task4QueensBattle();
+                    break;
+                case 5:
+                    printf("Goodbye!\n");
+                    break;
+                default:
+                    printf("Please choose a task number from the list.\n");
+                    break;
             }
-        }
-        else
-        {
-            scanf("%*s");
+        } else {
+            scanf("%*s"); // To discard the invalid input
         }
 
-    } while (task != 6);
+    } while (task != 5);
 }
 
-void task1RobotPaths()
-{
+void task1RobotPaths() {
     // Todo
 }
 
-void task2HumanPyramid()
-{
+void task2HumanPyramid() {
     // Todo
 }
 
-void task3ParenthesisValidator()
-{
+void task3ParenthesisValidator() {
     // Todo
 }
 
-void task4QueensBattle()
-{
-    // Todo
-}
-
-void task5CrosswordGenerator()
-{
+void task4QueensBattle() {
     // Todo
 }
